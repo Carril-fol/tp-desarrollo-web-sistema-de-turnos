d60e4d191c1219cdd9d3324ab566d3ec339ecb84
Folco Carril <folco.carril@gmail.com>
2024-08-23 16:50:22 -0300

    initial setup
    feat: Assets para su respectivo uso en las paginas.
    
    feat: Componentes para su uso en los diferente puntos de la pagina.
    Estos componentes permiten reutilizarlos en diferentes puntos de la aplicación.
    
    feat: Index.php se agrego para su uso,-Este contendra las secciones y diferentes enlaces a diferentes secciones.

---
+++ public/pages/index.php
@@ -0,0 +1,27 @@
+<?php
+    include("../components/common/header.html")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../components/App.css">
+    <title>Centro Clinico Vitalis</title>
+</head>
+<body>
+
+    <?php
+        include("../components/landingPage/whatWeDo.html")
+    ?>
+
+</body>
+</html>
+
+<?php
+    include("../components/common/footer.html")
+?>
