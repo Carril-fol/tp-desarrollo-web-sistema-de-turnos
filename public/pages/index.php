114f330109cdb77d1eebeac23e119cae544c2cd4
Ezequiel Mendoza <zekkimendoza@gmail.com>
2024-08-26 16:56:57 -0300

    Add Seccion Titulo + img + sticky nav + index detalles

--- public/pages/index.php
+++ public/pages/index.php
@@ -1,7 +1,3 @@
-<?php
-    include("../components/common/header.html")
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,12 +7,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../components/App.css">
+    <link rel="shortcut icon" href="..\assets\images\logo.png" type="image/x-icon">
     <title>Centro Clinico Vitalis</title>
 </head>
 <body>
-
     <?php
-        include("../components/landingPage/whatWeDo.html")
+        include("../components/landingPage/seccion-titulo.html");
+        include("../components/common/header.html");
+        include("../components/landingPage/whatWeDo.html");
     ?>
 
 </body>
