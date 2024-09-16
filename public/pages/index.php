6ddf94b9a6fd7e753a601d4be0970acb921767ea
Folco Carril <folco.carril@gmail.com>
2024-09-16 14:34:34 -0300

    refactor: se renombro a la seccion-whatWeDo

--- public/pages/index.php
+++ public/pages/index.php
@@ -7,21 +7,19 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../components/App.css">
-    <link rel="shortcut icon" href="..\assets\images\logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="..\assets\images\logo.webp" type="image/x-icon">
     <title>Centro Clinico Vitalis</title>
 </head>
 <body>
     <?php
         include("../components/landingPage/seccion-titulo.html");
         include("../components/common/header.html");
-        include("../components/landingPage/whatWeDo.html");
+        include("../components/landingPage/seccion-que-hacemos.html");
         include("../components/landingPage/seccion-nosotros.html");
         include("../components/landingPage/seccion-comentarios.html");
     ?>
-
 </body>
 </html>
-
 <?php
     include("../components/common/footer.html")
 ?>
