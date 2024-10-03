76708993115a5b5841772771f41cae11de08f5b8
Folco Carril <folco.carril@gmail.com>
2024-10-03 19:20:00 -0300

    feat(register): Se agrego una función de JS. Se agrego una funcion de JS para manejar dinamicamente los campos de los diferentes roles.
    feat(login): Se agrego logica de PHP para manejar las excepciones que produzca el codigo.

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,17 +1,25 @@
+<?php
+    session_start();
+    if (isset($_SESSION['error'])) {
+        echo "<div style='color: red;'>Error: " . "Dni o Contraseña invalida" . "</div>";
+        unset($_SESSION['error']);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Centro Clinico Vitalis</title>
     <link rel="stylesheet" href="../../css/App.css">
+    <link rel="shortcut icon" href="../../assets/images/logo.webp" type="image/x-icon">
 </head>
 <body class="formularios">
     <h1>Formulario de ingreso</h1>
-    <!-- <form action="../../controllers/auth/LoginController.php" method="POST"> -->
-    <form action="../../controllers/auth/LoginController2.php" method="POST">
+    <form action="../../controllers/auth/LoginController.php" method="POST">
         <input type="text" name="dni" placeholder="DNI"/>
-        <input type="text" name="password" placeholder="Password"/>
+        <input type="password" name="password" placeholder="Password"/>
         <input type="submit" value="Iniciar Sesion">
         <a href="register.php" style="color:white;">Registrar</a>
     </form>
--- views/auth/register.php
+++ views/auth/register.php
@@ -1,24 +1,36 @@
+<?php
+    session_start();
+    if (isset($_SESSION['error'])) {
+        echo "<div style='color: red;'>Error: " . $_SESSION['error'] . "</div>";
+        unset($_SESSION['error']);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Centro Clinico Vitalis</title>
     <link rel="stylesheet" href="../../css/App.css">
 </head>
 <body class="formularios">
     <h1>Formulario de Registro de usuario</h1>
-    <!-- <form action="../../controllers/auth/RegisterUsersController.php" method="POST"> -->
-    <form action="../../controllers/auth/RegisterUsersController2.php" method="POST">
+    <form action="../../controllers/auth/RegisterUsersController.php" method="POST">
         <input type="text" name="firstName" placeholder="Nombre"/>
         <input type="text" name="lastName" placeholder="Apellido"/>
         <input type="text" name="dni" placeholder="DNI" pattern="^\d{8}$" title="El DNI debe tener 8 dígitos." required/>
-        <select name="occupation"required>
+        
+        <select name="occupation" id="occupationSelect" required onchange="checkOccupation()">
             <option value="Seleccione una opcion">Seleccione una opcion</option>
             <option value="Medico">Medico</option>
             <option value="Administrativo">Administrativo</option>
             <option value="Paciente">Paciente</option>
         </select>
+
+        <div id="additionalInputs">
+        </div>
+        
         <input type="email" name="email" placeholder="Email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Ingrese un correo electrónico válido." required/>
         <input type="password" name="password" placeholder="Contraseña"  required/>
         <input type="password" name="confirmPassword" placeholder="Confirmar contraseña" required/>        
@@ -26,4 +38,31 @@
         <a href="login.php" style="color:white;">volver al Ingreso</a>
     </form>
 </body>
+
+<script>
+    function checkOccupation() {
+        const occupation = document.getElementById("occupationSelect").value;
+        const additionalInputs = document.getElementById("additionalInputs");
+
+        additionalInputs.innerHTML = ''
+
+        switch (occupation) {
+            case 'Medico':
+                additionalInputs.innerHTML = `
+                    <input type="text" name="medicLicense" placeholder="Número de licencia médica" required/>
+                    <input type="text" name="specialty" placeholder="Especialidad médica" required/>
+                `;
+                break;
+            case 'Paciente':
+                additionalInputs.innerHTML = `
+                    <input type="text" name="numberHealthInsurance" placeholder="Número de obra social" required/>
+                    <label for="isPartner">Es socio</label>
+                    <input type="checkbox" name="isPartner" required/>
+                `;
+        }
+    }
+    checkOccupation();
+</script>
+
 </html>
+
