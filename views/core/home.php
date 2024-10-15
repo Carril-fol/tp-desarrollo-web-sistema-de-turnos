be04cd4f2da2ed4bb971c8606fc78409a9bbc688
Folco Carril <folco.carril@gmail.com>
2024-10-15 19:15:26 -0300

    feat: Se agrego una home para los usuarios que se encuentren logeados.

--- views/core/home.php
+++ views/core/home.php
@@ -1,3 +1,15 @@
+<?php
+    // Imports
+    include("../../controllers/core/HomeController.php");
+
+    $homeController = new HomeController();
+    $hasCookie = $homeController->hasAccessTokenInCookies();
+    if (!$hasCookie) {
+        echo "<p>No tienes permiso para acceder a esta página. Por favor, inicia sesión.</p>";
+        header("Location: ../../views/auth/login.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,11 +17,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Centro Clinico Vitalis</title>
     <link rel="stylesheet" href="../../css/App.css">
+    <link rel="stylesheet" href="../../css/core/Core.css">
     <link rel="shortcut icon" href="../../assets/images/logo.webp" type="image/x-icon">
 </head>
 <body>
-    <?php
-        include("../../components/common/headerLogged.html");
-        ?>
+    <?php include("../../components/common/headerLogged.html");?>
+    <section class="seccion-home">
+        <div class="container-table">
+            <div class="container-buttons-table">
+                <button>Agregar</button>
+            </div>
+            <?php
+                require_once("../../components/turns/TurnTableComponent.php");
+                $turns = $homeController->showTurns();
+                renderTurnTable($turns);
+            ?>
+        </div>
+    </section>
 </body>
 </html>
