b02f3b901bfc15e18789c0872c00242cb36571bb
Folco Carril <folco.carril@gmail.com>
2024-10-16 07:01:48 -0300

    refactor: Se volvio a una versión anterior de RegisterUsersController

--- controllers/auth/RegisterUsersController.php
+++ controllers/auth/RegisterUsersController.php
@@ -1,106 +1,62 @@
 <?php
     // Importes
+    require '../../config.php';
     require '../../models/User.php';
-    require '../../models/Administrative.php';
-    require '../../models/Medic.php';
-    require '../../models/Patient.php';
 
-    class RegisterUsersController {
+    // Instancia de modelo de usuario
+    $userModel = new User;
 
-        private $userModel;
-        private $administrativeModel;
-        private $medicModel;
-        private $patientModel;
+    try {
+        $firstName = htmlentities(addslashes(strtoupper($_POST['firstName'])));
+        $lastName = htmlentities(addslashes(strtoupper($_POST['lastName'])));
+        $occupation = strtoupper($_POST['occupation']);
+        $dni = htmlentities(addslashes($_POST['dni']));
+        $email = strtolower($_POST['email']);
+        $password = htmlentities(addslashes($_POST['password']));
+        $confirmPassword = htmlentities(addslashes($_POST['confirmPassword']));
+        
+        if ($password != $confirmPassword) {
+            throw new Exception('Las contraseñas no son iguales.');
+        }
+        $passwordHashed = password_hash($confirmPassword, PASSWORD_DEFAULT);
 
-        function __construct() {
-            $this->userModel = new User;
-            $this->administrativeModel = new Administrative;
-            $this->medicModel = new Medic;
-            $this->patientModel = new Patient;
+        $userCreated = $userModel->createUser($dni, $firstName, $lastName, $email, $passwordHashed, false, false);
+        if (!$userCreated) {
+            throw new Exception('Error al crear el usuario.');
         }
 
-        private function sanitizeInput($input) {
-            return htmlentities(addslashes($input));
-        }
+        $getUserCreated = $userModel->getDataFromUserByDni($dni);
 
-        private function verifyPasswordsAreEqual() {
-            $password = $this->sanitizeInput($_POST['password']);
-            $confirmPassword = $this->sanitizeInput($_POST['confirmPassword']);
-            if ($password !== $confirmPassword) {
-                throw new Exception("Las contraseñas ingresadas no son iguales.");
-            }
+        switch ($occupation) {
+            case 'ADMINISTRATIVO':
+                require_once '../../models/Administrative.php';
+                $administrativeModel = new Administrative;
+                $administrativeModel->createAdministrative($dni);
+                break;
+            case 'MEDICO':
+                require_once '../../models/Medic.php';
+                $medicModel = new Medic;
+                $medicLicense = htmlentities(addslashes(strtoupper($_POST['medicLicense'])));
+                $medicSpeciality = htmlentities(addslashes(strtoupper($_POST['specialty'])));
+                $medicModel->createMedic(
+                    $dni,
+                    $medicLicense, 
+                    $medicSpeciality
+                );
+                break;
+            case 'PACIENTE':
+                require_once '../../models/Patient.php';
+                $patientModel = new Patient;
+                $patientModel->createPatient($dni);
+                break;
         }
+        header(header: "Location: ../../views/auth/register.php");
+        exit();
 
-        private function createUserBasedOnOccupation($dni) {
-            $occupation = $this->sanitizeInput($_POST['occupation']);
-            switch ($occupation) {
-                case 'ADMINISTRATIVO':
-                    $this->administrativeModel->createAdministrative($dni);
-                    break;
-                case 'MEDICO':
-                    $medicLicense = $this->sanitizeInput($_POST['medicLicense']);
-                    $medicSpeciality = $this->$_POST['specialty'];
-                    $this->medicModel->createMedic(
-                        $$dni, 
-                        $medicLicense, 
-                        $medicSpeciality
-                    );
-                    break;
-                case 'PACIENTE':
-                    $patientNumberHealthInsurance = $this->sanitizeInput($_POST['numberHealthInsurance']);
-                    $patientIsPartner = $_POST['isPartner'];
-                    $this->patientModel->createPatient(
-                        $$dni, 
-                        $patientNumberHealthInsurance, 
-                        $patientIsPartner
-                    );
-                    break;
-            }
-        }
-
-        private function createUser() {
-            $firstName = $this->sanitizeInput($_POST['firstName']);
-            $lastName = $this->sanitizeInput($_POST['lastName']);
-            $dni = $this->sanitizeInput($_POST['dni']);
-            $email = $this->sanitizeInput($_POST['email']);
-            $confirmPassword = $this->sanitizeInput($_POST['confirmPassword']);
-            $passwordHashed = password_hash($confirmPassword, PASSWORD_DEFAULT);
-            
-            $userCreated = $this->userModel->createUser(
-                $dni, 
-                $firstName, 
-                $lastName, 
-                $email, 
-                $passwordHashed, 
-                false, 
-                false
-            );
-            if (!$userCreated) {
-                throw new Exception("Error al crear el usuario");
-            }
-            return $dni;
-        }
-
-        private function handleError($error) {
-            session_start();
-            $_SESSION['error'] = $error->getMessage();
-            header("Location: ../../views/auth/register.php");
-            exit();
-        }
-
-        public function register() {
-            try {
-                $this->verifyPasswordsAreEqual();
-
-                $userCreatedDni = $this->createUser();
-                
-                $this->createUserBasedOnOccupation($userCreatedDni);
-            } catch (Exception $error) {
-                $this->handleError($error);
-            }
-        }
+    } catch (Exception $e) {
+        $_SESSION['error'] = $e->getMessage();
+        header(header: "Location: ../../views/auth/register.php");
+        exit();
     }
-
-    $registerUsersController = new RegisterUsersController;
-    $registerUsersController->register();
+    
 ?>
