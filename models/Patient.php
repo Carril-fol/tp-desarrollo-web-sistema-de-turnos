197931ec5a5613b30db4167bf948cbac35ea8921
Folco Carril <folco.carril@gmail.com>
2024-10-16 06:59:27 -0300

    refactor: Se hizo un cambio en el modelo de Patient

--- models/Patient.php
+++ models/Patient.php
@@ -13,14 +13,12 @@ class Patient
         $this->userModel = new User;
     }
 
-    public function createPatient(int $idUser, string $numberHealthInsurance, bool $isPartner)
+    public function createPatient(int $dni)
     {
-        $insertQuery = "INSERT INTO paciente (id_user, numero_de_obra_social, es_socio) 
-            VALUES (:id_user, :numero_de_obra_social, :es_socio)";
+        $insertQuery = "INSERT INTO paciente (dni) 
+            VALUES (:dni)";
         $stmt = $this->db->prepare($insertQuery);
-        $stmt->bindParam(':id_user', $idUser, PDO::PARAM_INT);
-        $stmt->bindParam(':numero_de_obra_social', $numberHealthInsurance, PDO::PARAM_STR);
-        $stmt->bindParam(':es_socio', $isPartner, PDO::PARAM_BOOL);
+        $stmt->bindParam(':dni', $dni, PDO::PARAM_INT);
         $resultQuery = $stmt->execute();
         return $resultQuery;
     }
@@ -28,9 +26,9 @@ class Patient
     public function getPatientByDni($dni)
     {
         $idUser = $this->userModel->getDataFromUserByDni($dni);
-        $selectQuery = "SELECT * FROM paciente WHERE id_user = :id_user";
+        $selectQuery = "SELECT * FROM paciente WHERE dni = :dni";
         $stmt = $this->db->prepare($selectQuery);
-        $stmt->bindParam(':id_user', $idUser, PDO::PARAM_INT);
+        $stmt->bindParam(':dni', $dni, PDO::PARAM_INT);
         $resultQuery = $stmt->execute();
         return $resultQuery;
     }
