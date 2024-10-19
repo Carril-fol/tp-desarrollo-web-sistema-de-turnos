93fcdd096a807a0d6377c9b788d19c17c0aa30cf
Folco Carril <folco.carril@gmail.com>
2024-10-19 07:39:46 -0300

    refactor: Se agrego el redireccionamiento en la tabla de turnos.

--- components/turns/TurnTableComponent.php
+++ components/turns/TurnTableComponent.php
@@ -1,64 +1,60 @@
 <?php
-    function renderTurnTable($turns) {
-        if (empty($turns)) {
-            echo "
-                <div>
-                    No hay turnos por el momento a mostrar
-                </div>
-            ";
-        } else {
-            echo "
-                <div class='container-table-button-top'>
-                    <a href>
-                        <button class='table-button-add'>
-                            Agregar
-                        </button>
-                    </a>
-                </div>
-            ";
-            echo "<table class='content-table'>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>DNI - Paciente</th>
-                            <th>DNI - Medico</th>
-                            <th>Fecha atención</th>
-                            <th>Fecha creación</th>
-                            <th>Horario</th>
-                            <th>Estado</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>";
+function renderTurnTable($turns) {
+    if (empty($turns)) {
+        echo "
+            <div>
+                No hay turnos por el momento a mostrar
+            </div>
+        ";
+    } else {
+        echo "
+            <div class='container-table-button-top'>
+                <a href='../../views/turn/createTurn.php'>
+                    <button class='table-button-add'>
+                        Agregar
+                    </button>
+                </a>
+            </div>
+        ";
+        echo "<table class='content-table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>DNI - Paciente</th>
+                        <th>DNI - Medico</th>
+                        <th>Fecha atención</th>
+                        <th>Fecha creación</th>
+                        <th>Horario</th>
+                        <th>Estado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>";
 
-            foreach ($turns as $row) {
-                echo 
-                    "<tr>
-                            <td>" . htmlspecialchars($row['id']) . "</td>
-                            <td>" . htmlspecialchars($row['dni_paciente']) . "</td>
-                            <td>" . htmlspecialchars($row['dni_medico']) . "</td>
-                            <td>" . htmlspecialchars($row['fecha_atencion']) . "</td>
-                            <td>" . htmlspecialchars($row['fecha_creacion']) . "</td>
-                            <td>" . htmlspecialchars($row['horario']) . "</td>
-                            <td>" . htmlspecialchars($row['estado']) . "</td>
-                            <td>
-                                <div class'container-buttons-table-aside'>
-                                        <a>
-                                            <button class='table-button-update' type='submit'>
-                                                Editar
-                                            </button>
-                                        </a>
-                                    <a>
-                                        <button class='table-button-delete' type='submit'>
-                                            Borrar
-                                        </button>
-                                    </a>
-                                </div>
-                            </td>
-                    </tr>";
-            }
-            echo "  </tbody>
-                </table>";
+        foreach ($turns as $row) {
+            echo 
+                "<tr>
+                    <td>" . htmlspecialchars($row['id']) . "</td>
+                    <td>" . htmlspecialchars($row['dni_paciente']) . "</td>
+                    <td>" . htmlspecialchars($row['dni_medico']) . "</td>
+                    <td>" . htmlspecialchars($row['fecha_atencion']) . "</td>
+                    <td>" . htmlspecialchars($row['fecha_creacion']) . "</td>
+                    <td>" . htmlspecialchars($row['horario']) . "</td>
+                    <td>" . htmlspecialchars($row['estado']) . "</td>
+                    <td>
+                        <div class='container-buttons-table-aside'>
+                            <a href='../../controllers/turn/TurnController.php?action=update&id=" . htmlspecialchars($row['id']) . "'>
+                                <button class='table-button-update' type='button'>Editar</button>
+                            </a>
+                            <a href='../../controllers/turn/TurnController.php?action=delete&id=" . htmlspecialchars($row['id']) . "'>
+                                <button class='table-button-delete' type='button'>Eliminar</button>
+                            </a>
+                        </div>
+                    </td>
+                </tr>";
         }
+        echo "  </tbody>
+            </table>";
     }
+}
 ?>
