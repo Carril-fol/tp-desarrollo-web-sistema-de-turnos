61ee248de15c2f7562e22cb2294104b94c49a767
Folco Carril <folco.carril@gmail.com>
2024-10-19 07:43:44 -0300

    refactor: Se borro el codigo JS del .php, para realizar su importación desde un archivo.

--- views/auth/register.php
+++ views/auth/register.php
@@ -68,27 +68,5 @@
         include("../../components/common/footer.html");
     ?>
 </body>
-
-<script>
-    function checkOccupation() {
-        const occupation = document.getElementById("occupationSelect").value;
-        const additionalInputs = document.getElementById("additionalInputs");
-
-        additionalInputs.innerHTML = ''
-
-        switch (occupation) {
-            case 'Medico':
-                additionalInputs.innerHTML = `
-                    <div class="contenedor-opciones">
-                        <input type="text" name="medicLicense" placeholder="Número de licencia médica" required/>
-                        <input type="text" name="specialty" placeholder="Especialidad médica" required/>
-                    </div>
-                `;
-                break;
-        }
-    }
-    checkOccupation();
-</script>
-
 </html>
 
